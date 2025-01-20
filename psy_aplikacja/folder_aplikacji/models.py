9f9c39b2b6a7307a0935a7c98340c4b182ddd50b
Olek <aleksander.smialowski@outlook.com>
2025-01-20 16:10:29 +0100

    dodane modele koty/psy

--- psy_aplikacja/folder_aplikacji/models.py
+++ psy_aplikacja/folder_aplikacji/models.py
@@ -6,15 +6,21 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # deklaracja statycznej listy wyboru do wykorzystania w klasie modelu
 MONTHS = models.IntegerChoices('Miesiace', 'Styczeń Luty Marzec Kwiecień Maj Czerwiec Lipiec Sierpień Wrzesień Październik Listopad Grudzień')
 
-DOG_SIZE = (
+SIZE = (
         ('S', 'Small'),
         ('M', 'Medium'),
         ('L', 'Large'),
     )
 
 AGE_CHOICES =[
+    ('kitten', 'Kociak'),
     ('puppy', 'Szczeniak'),
     ('age', 'Wiek')
+]
+
+CASTRATED_CHOICES =[
+    ('YES','TAK'),
+    ('NO','NIE')
 ]
 
 
@@ -29,15 +35,33 @@ class Team(models.Model):
 class Dog(models.Model):
 
     name = models.CharField(max_length=60) #imie psa
-    DOG_SIZE = models.CharField(max_length=1, choices=DOG_SIZE, default=DOG_SIZE[0][0]) #wielkosc psa
+    rasa_psa = models.CharField(max_length=60) # rasa psa
+    SIZE = models.CharField(max_length=1, choices=SIZE, default=SIZE[0][0]) #wielkosc psa
     month_added = models.IntegerField(choices=MONTHS.choices, default=MONTHS.choices[0][0]) #miesiac dodania ogloszenia
     team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL) #gotowy do adopcji lub nie 
     age_type = models.CharField(max_length=10, choices=AGE_CHOICES, default='age') #wiek typ
     age = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(0),MaxValueValidator(99)]) # wiek
+    castrated = models.CharField(max_length=10, choices=CASTRATED_CHOICES, default="") #czy pies jest wykastrowany
+
+class Cat(models.Model):
+
+    name = models.CharField(max_length=60) #imie kota
+    rasa_kota = models.CharField(max_length=60) # rasa kota
+    SIZE = models.CharField(max_length=1, choices=SIZE, default=SIZE[0][0]) #wielkosc kota
+    month_added = models.IntegerField(choices=MONTHS.choices, default=MONTHS.choices[0][0]) #miesiac dodania ogloszenia
+    team = models.ForeignKey(Team, null=True, blank=True, on_delete=models.SET_NULL) #gotowy do adopcji lub nie 
+    age_type = models.CharField(max_length=10, choices=AGE_CHOICES, default='age') #wiek typ
+    age = models.IntegerField(blank=True, null=True, validators=[MinValueValidator(0),MaxValueValidator(99)]) # wiek
+    castrated = models.CharField(max_length=10, choices=CASTRATED_CHOICES, default="") #czy kot jest wykastrowany
+
+
+
 
     def __str__(self):
         if self.age_type == 'puppy' :
             return "Szczeniak"
+        if self.age_type == 'kitten' :
+            return "Kociak"
         return f"Wiek: {self.age} lat"
 
     def __str__(self):
