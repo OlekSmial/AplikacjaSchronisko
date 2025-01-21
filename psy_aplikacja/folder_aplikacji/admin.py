5d36f52547dfc8fb171042eb593438d16efa7e86
Olek <aleksander.smialowski@outlook.com>
2025-01-21 19:52:09 +0100

    narazie wszystkie modele

--- psy_aplikacja/folder_aplikacji/admin.py
+++ psy_aplikacja/folder_aplikacji/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Team, Dog, Cat
+from .models import Dog, Cat, User, Money_collection, Shelter, Cage
 
-admin.site.register(Team)
 admin.site.register(Dog)
 admin.site.register(Cat)
+admin.site.register(User)
+admin.site.register(Money_collection)
+admin.site.register(Shelter)
+admin.site.register(Cage)
